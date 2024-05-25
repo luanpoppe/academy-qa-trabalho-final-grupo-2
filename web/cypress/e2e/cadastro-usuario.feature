0394f08d929aeb42b78d4461d67b48b7836f1473
luanpoppe <luanpoppe@gmail.com>
2024-05-25 16:56:11 -0300

    feat: adicionando arquivos do cypress e configurando o cucumber

---
+++ web/cypress/e2e/cadastro-usuario.feature
@@ -0,0 +1,6 @@
+# language: pt
+
+Funcionalidade: Validar a criação de usuários
+
+Cenário: Cenário de tetse
+  Dado que eu fiz algo
---
+++ web/cypress/support/steps/cadastro-usuario.step.js
@@ -0,0 +1,5 @@
+import { Before, After, Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('que eu fiz algo', function () {
+
+})
