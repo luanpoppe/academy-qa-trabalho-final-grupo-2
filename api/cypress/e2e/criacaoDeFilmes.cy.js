1300a246933c4d5aebe6bcee116d3a925cf1da72
luanpoppe <luanpoppe@gmail.com>
2024-05-31 00:16:10 -0300

    feat: adicionado testes de criação de filmes

--- api/cypress/e2e/criacaoDeFilmes.cy.js
+++ api/cypress/e2e/criacaoDeFilmes.cy.js
@@ -25,32 +25,274 @@ describe('Criação de Filmes', function () {
       })
     })
     after(function () {
-      if (movieInfo) cy.promoteToAdminAndDeleteMovie(adminUser, movieInfo.id)
       cy.deleteUser(adminUser)
     })
 
-    it('Usuário administrador deve poder criar um filme', function () {
-      cy.request({
-        method: "POST",
-        url: "/api/movies",
-        body: movie,
-        auth: {
-          bearer: token
-        },
-      }).then(function (resposta) {
-        movieInfo = resposta.body
-        expect(resposta.status).to.equal(201)
-        expect(resposta.body).to.deep.include(movie)
-        expect(resposta.body).to.have.property("id")
+    describe('Casos de criação com sucesso', function () {
+      afterEach(function () {
+        if (movieInfo) cy.promoteToAdminAndDeleteMovie(adminUser, movieInfo.id)
+      })
+
+      it('Usuário administrador deve poder criar um filme', function () {
+        cy.request({
+          method: "POST",
+          url: "/api/movies",
+          body: movie,
+          auth: {
+            bearer: token
+          },
+        }).then(function (resposta) {
+          movieInfo = resposta.body
+          expect(resposta.status).to.equal(201)
+          expect(resposta.body).to.deep.include(movie)
+          expect(resposta.body).to.have.property("id")
+        })
+      })
+
+      it('Deve ser possível criar um filme com o título contendo 1 caractere', function () {
+        const temporaryMovie = {
+          ...movie,
+          title: "a"
+        }
+
+        cy.request({
+          method: "POST",
+          url: "/api/movies",
+          body: temporaryMovie,
+          auth: {
+            bearer: token
+          },
+        }).then(function (resposta) {
+          movieInfo = resposta.body
+          expect(resposta.status).to.equal(201)
+        })
+      })
+
+      it('Deve ser possível criar um filme com o título contendo 100 caracteres', function () {
+        const temporaryMovie = {
+          ...movie,
+          title: "a"
+        }
+        while (temporaryMovie.title.length < 100) {
+          temporaryMovie.title += "a"
+        }
+
+        cy.request({
+          method: "POST",
+          url: "/api/movies",
+          body: temporaryMovie,
+          auth: {
+            bearer: token
+          },
+        }).then(function (resposta) {
+          movieInfo = resposta.body
+          expect(resposta.status).to.equal(201)
+        })
       })
     })
 
-    it.only('Não deve ser possível criar um filme sem um título ', function () {
+
+    describe('Casos de falha do título do filme', function () {
+      it('Não deve ser possível criar um filme sem um título', function () {
+        const temporaryMovie = {
+          ...movie,
+          title: null
+        }
+
+        cy.request({
+          method: "POST",
+          url: "/api/movies",
+          body: temporaryMovie,
+          auth: {
+            bearer: token
+          },
+          failOnStatusCode: false
+        }).then(function (resposta) {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.have.length(3)
+          cy.wrap(movieErrors.titleNonExistentErrors).each(function (error) {
+            expect(resposta.body.message).to.deep.include(error)
+          })
+        })
+      })
+
+      it('Não deve ser possível criar um filme com um título vazio', function () {
+        const temporaryMovie = {
+          ...movie,
+          title: ""
+        }
+
+        cy.request({
+          method: "POST",
+          url: "/api/movies",
+          body: temporaryMovie,
+          auth: {
+            bearer: token
+          },
+          failOnStatusCode: false
+        }).then(function (resposta) {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.have.length(2)
+          expect(resposta.body.message).to.deep.include(movieErrors.titleErrors.titleMustBeLonger)
+          expect(resposta.body.message).to.deep.include(movieErrors.titleErrors.titleMustNotBeEmpty)
+        })
+      })
+
+      it('Não deve ser possível criar um filme com um título sendo um número', function () {
+        const temporaryMovie = {
+          ...movie,
+          title: 1234
+        }
+
+        cy.request({
+          method: "POST",
+          url: "/api/movies",
+          body: temporaryMovie,
+          auth: {
+            bearer: token
+          },
+          failOnStatusCode: false
+        }).then(function (resposta) {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.have.length(2)
+          expect(resposta.body.message).to.deep.include(movieErrors.titleErrors.titleMustBeShortherAndLonger)
+          expect(resposta.body.message).to.deep.include(movieErrors.titleErrors.titleMustBeString)
+        })
+      })
+
+      it('Não deve ser possível criar um filme com um título contendo 101 caracteres', function () {
+        const temporaryMovie = {
+          ...movie,
+          title: "a"
+        }
+
+        while (temporaryMovie.title.length < 101) {
+          temporaryMovie.title += "a"
+        }
+
+        cy.request({
+          method: "POST",
+          url: "/api/movies",
+          body: temporaryMovie,
+          auth: {
+            bearer: token
+          },
+          failOnStatusCode: false
+        }).then(function (resposta) {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.have.length(1)
+          expect(resposta.body.message).to.deep.include(movieErrors.titleErrors.titleMustBeShorter)
+        })
+      })
+    })
+
+    describe('Casos de falha do gênero do filme', function () {
+      it('Não deve ser possível criar um filme sem passar um gênero', function () {
+        const temporaryMovie = {
+          ...movie,
+          genre: null
+        }
+
+        cy.request({
+          method: "POST",
+          url: "/api/movies",
+          body: temporaryMovie,
+          auth: {
+            bearer: token
+          },
+          failOnStatusCode: false
+        }).then(function (resposta) {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.have.length(3)
+          cy.wrap(movieErrors.allGenreErrors).each(function (error) {
+            expect(resposta.body.message).to.deep.include(error)
+          })
+        })
+      })
+
+      it('Não deve ser possível criar um filme com gênero vazio', function () {
+        const temporaryMovie = {
+          ...movie,
+          genre: ""
+        }
+
+        cy.request({
+          method: "POST",
+          url: "/api/movies",
+          body: temporaryMovie,
+          auth: {
+            bearer: token
+          },
+          failOnStatusCode: false
+        }).then(function (resposta) {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.have.length(2)
+          expect(resposta.body.message).to.deep.include(movieErrors.genreErrors.genreMustBeLonger)
+          expect(resposta.body.message).to.deep.include(movieErrors.genreErrors.genreMustNotBeEmpty)
+        })
+      })
+
+      it('Não deve ser possível criar um filme com gênero sendo um número', function () {
+        const temporaryMovie = {
+          ...movie,
+          genre: 1234
+        }
+
+        cy.request({
+          method: "POST",
+          url: "/api/movies",
+          body: temporaryMovie,
+          auth: {
+            bearer: token
+          },
+          failOnStatusCode: false
+        }).then(function (resposta) {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.have.length(2)
+          expect(resposta.body.message).to.deep.include(movieErrors.genreErrors.genreMustBeShortherAndLonger)
+          expect(resposta.body.message).to.deep.include(movieErrors.genreErrors.genreMustBeString)
+        })
+      })
+
+      it('Não deve ser possível criar um filme com um gênero contendo 101 caracteres', function () {
+        const temporaryMovie = {
+          ...movie,
+          genre: "a"
+        }
+
+        while (temporaryMovie.genre.length < 101) {
+          temporaryMovie.genre += "a"
+        }
+
+        cy.log('temporaryMovie.genre', temporaryMovie.genre)
+        cy.log('temporaryMovie.genre', temporaryMovie.genre.length)
+
+
+        cy.request({
+          method: "POST",
+          url: "/api/movies",
+          body: temporaryMovie,
+          auth: {
+            bearer: token
+          },
+          failOnStatusCode: false
+        }).then(function (resposta) {
+          expect(resposta.status).to.equal(400)
+          expect(resposta.body.message).to.have.length(1)
+          expect(resposta.body.message).to.deep.include(movieErrors.genreErrors.genreMustBeShorter)
+        })
+      })
+    })
+
+
+
+
+
+    it('Não deve ser possível criar um filme sem uma descrição', function () {
       const temporaryMovie = {
         ...movie,
-        title: null
+        description: null
       }
-      const tituloErros = movieErrors.titleErrors
 
       cy.request({
         method: "POST",
@@ -63,7 +305,55 @@ describe('Criação de Filmes', function () {
       }).then(function (resposta) {
         expect(resposta.status).to.equal(400)
         expect(resposta.body.message).to.have.length(3)
-        expect(resposta.body.message).to.include(...[tituloErros.titleMustBeLonger, tituloErros.titleMustBeString, tituloErros.titleMustNotBeEmpty])
+        cy.wrap(movieErrors.allDescriptionErrors).each(function (error) {
+          expect(resposta.body.message).to.deep.include(error)
+        })
+      })
+    })
+
+    it('Não deve ser possível criar um filme sem uma duração', function () {
+      const temporaryMovie = {
+        ...movie,
+        durationInMinutes: null
+      }
+
+      cy.request({
+        method: "POST",
+        url: "/api/movies",
+        body: temporaryMovie,
+        auth: {
+          bearer: token
+        },
+        failOnStatusCode: false
+      }).then(function (resposta) {
+        expect(resposta.status).to.equal(400)
+        expect(resposta.body.message).to.have.length(5)
+        cy.wrap(movieErrors.allDurationErrors).each(function (error) {
+          expect(resposta.body.message).to.deep.include(error)
+        })
+      })
+    })
+
+    it('Não deve ser possível criar um filme sem um ano de lançamento', function () {
+      const temporaryMovie = {
+        ...movie,
+        releaseYear: null
+      }
+
+      cy.request({
+        method: "POST",
+        url: "/api/movies",
+        body: temporaryMovie,
+        auth: {
+          bearer: token
+        },
+        failOnStatusCode: false
+      }).then(function (resposta) {
+        expect(resposta.status).to.equal(400)
+        expect(resposta.body.message).to.have.length(5)
+        cy.wrap(movieErrors.allReleaseYearErrors).each(function (error) {
+          expect(resposta.body.message).to.deep.include(error)
+        })
       })
     })
   })
--- api/cypress/support/utils/movieErrorsClass.js
+++ api/cypress/support/utils/movieErrorsClass.js
@@ -1,7 +1,62 @@
 export class MovieErrors {
     titleErrors = {
         titleMustBeLonger: "title must be longer than or equal to 1 characters",
+        titleMustBeShorter: "title must be shorter than or equal to 100 characters",
         titleMustBeString: "title must be a string",
-        titleMustNotBeEmpty: "title should not be empty"
+        titleMustNotBeEmpty: "title should not be empty",
+        titleMustBeShortherAndLonger: "title must be longer than or equal to 1 and shorter than or equal to 100 characters",
     }
+
+    titleNonExistentErrors = [this.titleErrors.titleMustBeLonger, this.titleErrors.titleMustBeString, this.titleErrors.titleMustNotBeEmpty]
+
+    genreErrors = {
+        genreMustBeLonger: "genre must be longer than or equal to 1 characters",
+        genreMustBeString: "genre must be a string",
+        genreMustNotBeEmpty: "genre should not be empty",
+        genreMustBeShorter: "genre must be shorter than or equal to 100 characters",
+        genreMustBeShortherAndLonger: "genre must be longer than or equal to 1 and shorter than or equal to 100 characters"
+    }
+
+    allGenreErrors = [this.genreErrors.genreMustBeLonger, this.genreErrors.genreMustBeString, this.genreErrors.genreMustNotBeEmpty]
+
+    descriptionErrors = {
+        descriptionMustBeLonger: "description must be longer than or equal to 1 characters",
+        descriptionMustBeString: "description must be a string",
+        descriptionMustNotBeEmpty: "description should not be empty"
+    }
+
+    allDescriptionErrors = [this.descriptionErrors.descriptionMustBeLonger, this.descriptionErrors.descriptionMustBeString, this.descriptionErrors.descriptionMustNotBeEmpty]
+
+    durationErrors = {
+        durationMaxNumber: "durationInMinutes must not be greater than 43200",
+        durationMinNumber: "durationInMinutes must not be less than 1",
+        durationMustBeNumber: "durationInMinutes must be a number conforming to the specified constraints",
+        durationMustBeInteger: "durationInMinutes must be an integer number",
+        durationMustNotBeEmpty: "durationInMinutes should not be empty"
+    }
+
+    allDurationErrors = [
+        this.durationErrors.durationMaxNumber,
+        this.durationErrors.durationMinNumber,
+        this.durationErrors.durationMustBeNumber,
+        this.durationErrors.durationMustBeInteger,
+        this.durationErrors.durationMustNotBeEmpty
+    ]
+
+    releaseYearErrors = {
+        releaseYearMaxNumber: "releaseYear must not be greater than 2024",
+        releaseYearMinNumber: "releaseYear must not be less than 1895",
+        releaseYearMustBeInteger: "releaseYear must be an integer number",
+        releaseYearMustBeNumber: "releaseYear must be a number conforming to the specified constraints",
+        releaseYearMustNotBeEmpty: "releaseYear should not be empty"
+    }
+
+    allReleaseYearErrors = [
+        this.releaseYearErrors.releaseYearMaxNumber,
+        this.releaseYearErrors.releaseYearMinNumber,
+        this.releaseYearErrors.releaseYearMustBeInteger,
+        this.releaseYearErrors.releaseYearMustBeNumber,
+        this.releaseYearErrors.releaseYearMustNotBeEmpty
+    ]
+
 }
