5c3d194a236ed46fe72563d4c8178e763adec373
Maria Cristina <fernandesmc1@hotmail.com>
2024-06-07 20:35:10 -0300

    Alterações - Evolução do Perfil Crítico

--- api/cypress/e2e/EvoluçãoPerfilAdministrador.cy.js
+++ api/cypress/e2e/EvoluçãoPerfilAdministrador.cy.js
@@ -168,8 +168,7 @@ describe('Evolução de usuário para perfil Administrador', function () {
                                     expect(response.status).to.equal(200);
                                     expect(response.body).to.be.an("array");
                                     expect(response.body[0].reviewType).to.equal(0);
-                                    expect(response.body[1].reviewType).to.equal("1");
-
+                                    expect(response.body[1].reviewType).to.equal(1);
                                 })
                             })
                         })
