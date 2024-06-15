133091ac1e20f3126e0502c6a5dd18bb52989e46
luanpoppe <luanpoppe@gmail.com>
2024-06-15 14:58:13 -0300

    feat: revisão dos testes de exclusão de usuários pela API, com algumas melhorias e pequenos fixs

--- api/cypress/e2e/ExclusaodeUsuarios.cy.js
+++ api/cypress/e2e/ExclusaodeUsuarios.cy.js
@@ -2,163 +2,146 @@
 ///  <reference path="../support/index.d.ts" />
 
 describe('Exclusão de Usuários', function () {
-    var usuarioCriado;
-    var usuarioExcluido;
-    var token;
-    var filmeCriado;
-
-    before(function () {
-        cy.createUser().then((newUser) => {
-            usuarioExcluido = newUser;
-        });
-    });
+    let usuarioCriado;
+    let filmeCriado;
 
     beforeEach(function () {
         cy.createUser().then((newUser) => {
-            usuarioCriado = newUser;
+            usuarioCriado = newUser
+            cy.login(usuarioCriado).then(function (resposta) {
+                usuarioCriado.accessToken = resposta.body.accessToken
+            })
         });
     });
 
-    it('Não deve ser possível realizar exclusão de uma conta, sem efetuar login ', function () {
-        cy.request({
-            method: 'DELETE',
-            url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users/' + usuarioCriado.id,
-            failOnStatusCode: false,
-        }).then((response) => {
-            expect(response.status).to.equal(401);
-            expect(response.body.message).to.be.eq('Access denied.');
-            expect(response.body.error).to.be.eq('Unauthorized');
-        });
-    });
+    describe('Cenários com exclusão de usuário após o fim dos testes', function () {
+        afterEach(function () {
+            cy.deleteUser(usuarioCriado)
+        })
 
-
-    it('Não deve ser possível realizar exclusão de uma conta, sendo um usuário com perfil Comum', function () {
-        cy.login(usuarioCriado).then((login) => {
-            token = login.body.accessToken
-        }).then(function () {
+        it('Não deve ser possível realizar exclusão de uma conta, sem efetuar login ', function () {
             cy.request({
                 method: 'DELETE',
-                url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users/' + usuarioCriado.id,
+                url: '/api/users/' + usuarioCriado.id,
                 failOnStatusCode: false,
             }).then((response) => {
                 expect(response.status).to.equal(401);
-                expect(response.body.message).to.be.eq('Access denied.');
-                expect(response.body.error).to.be.eq('Unauthorized');
-            });
-        });
-    });
-
-    it('Não deve ser possível realizar exclusão de uma conta, sendo um usuário com perfil Crítico', function () {
-        cy.login(usuarioCriado).then((login) => {
-            token = login.body.accessToken
-        }).then(function () {
-            cy.promoteCritic(token).then(function () {
-                cy.request({
-                    method: 'DELETE',
-                    url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users/' + usuarioCriado.id,
-                    failOnStatusCode: false,
-                }).then((response) => {
-                    expect(response.status).to.equal(401);
-                    expect(response.body.message).to.be.eq('Access denied.');
-                    expect(response.body.error).to.be.eq('Unauthorized');
+                expect(response.body).to.deep.include({
+                    message: 'Access denied.',
+                    error: 'Unauthorized'
                 });
             });
         });
-    });
+
+        it('Não deve ser possível realizar exclusão de uma conta, sendo um usuário com perfil Comum', function () {
+            cy.request({
+                method: 'DELETE',
+                url: '/api/users/' + usuarioCriado.id,
+                failOnStatusCode: false,
+                auth: {
+                    bearer: usuarioCriado.accessToken
+                }
+            }).then((response) => {
+                expect(response.status).to.equal(403);
+                expect(response.body.message).to.equal("Forbidden");
+            });
+        });
+
+        it('Não deve ser possível realizar exclusão de uma conta, sendo um usuário com perfil Crítico', function () {
+            cy.promoteCritic(usuarioCriado.accessToken).then(function () {
+                cy.request({
+                    method: 'DELETE',
+                    url: '/api/users/' + usuarioCriado.id,
+                    failOnStatusCode: false,
+                    auth: {
+                        bearer: usuarioCriado.accessToken
+                    }
+                }).then((response) => {
+                    expect(response.status).to.equal(403);
+                    expect(response.body.message).to.equal("Forbidden");
+                });
+            });
+        });
+
+        it('Não deve ser possível que um usuário com perfil Administrador, insira um valor diferente de um número inteiro para excluir uma conta', function () {
+            cy.promoteAdmin(usuarioCriado.accessToken).then(function () {
+                cy.request({
+                    method: 'DELETE',
+                    url: '/api/users/' + "string",
+                    failOnStatusCode: false,
+                    auth: { bearer: usuarioCriado.accessToken },
+                }).then((response) => {
+                    expect(response.status).to.equal(400);
+                    expect(response.body).to.deep.include({
+                        error: "Bad Request",
+                        message: "Validation failed (numeric string is expected)"
+                    });
+                });
+            });
+        });
+
+        it('Excluir um usuário com id não existente deve retornar sucesso', function () {
+            cy.promoteAdmin(usuarioCriado.accessToken).then(function () {
+                cy.listAllUsers(usuarioCriado.accessToken).then(function (resposta) {
+                    const ultimoId = resposta.body[resposta.body.length - 1].id
+                    cy.request({
+                        method: 'DELETE',
+                        url: '/api/users/' + (ultimoId + 1000).toString(),
+                        failOnStatusCode: false,
+                        auth: {
+                            bearer: usuarioCriado.accessToken,
+                        },
+                    }).then((response) => {
+                        expect(response.status).to.equal(204);
+                        expect(response.body).to.equal("");
+                    });
+                })
+            });
+        });
+    })
 
     it('Deve ser possível que um usuário com perfil Administrador, exclua a própria conta', function () {
-        cy.deleteUser(usuarioCriado).then((response) => {
-            expect(response.status).to.equal(204);
-            expect(response.body).to.equal("");
-        });
+        cy.promoteAdmin(usuarioCriado.accessToken).then(function () {
+            cy.request({
+                method: 'DELETE',
+                url: '/api/users/' + usuarioCriado.id,
+                failOnStatusCode: false,
+                auth: { bearer: usuarioCriado.accessToken },
+            }).then((response) => {
+                expect(response.status).to.equal(204);
+                expect(response.body).to.equal("");
+            });
+        })
     });
 
     it('Deve ser possível que um usuário com perfil Administrador, exclua a conta de outro usuário', function () {
-
-        cy.login(usuarioCriado).then((login) => {
-            token = login.body.accessToken
-        }).then(function () {
-            cy.promoteAdmin(token).then(function () {
+        cy.createUser().then(function (resposta) {
+            const outroUsuario = resposta
+            cy.promoteAdmin(usuarioCriado.accessToken).then(function () {
                 cy.request({
                     method: 'DELETE',
-                    url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users/' + usuarioExcluido.id,
+                    url: '/api/users/' + outroUsuario.id,
                     auth: {
-                        bearer: token,
+                        bearer: usuarioCriado.accessToken,
                     },
                 }).then((response) => {
                     expect(response.status).to.equal(204);
                     expect(response.body).to.equal("");
                 });
             });
-        });
-    });
-
-    it('Não deve ser possível que um usuário com perfil Administrador, insira um valor diferente de um número inteiro para excluir uma conta', function () {
-
-        cy.login(usuarioCriado).then((login) => {
-            token = login.body.accessToken
-        }).then(function () {
-            cy.promoteAdmin(token).then(function () {
-                cy.request({
-                    method: 'DELETE',
-                    url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users/' + "string",
-                    failOnStatusCode: false,
-                    auth: {
-                        bearer: token,
-                    },
-                }).then((response) => {
-                    expect(response.status).to.equal(400);
-                    expect(response.body.error).to.equal("Bad Request");
-                    expect(response.body.message).to.equal("Validation failed (numeric string is expected)");
-                });
-            });
-        });
-    });
-
-    it('Excluir um usuário com id não existente deve retornar sucesso', function () {
-
-        cy.login(usuarioCriado).then((login) => {
-            token = login.body.accessToken
-        }).then(function () {
-            cy.promoteAdmin(token).then(function () {
-                cy.request({
-                    method: 'DELETE',
-                    url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users/' + 123172398172,
-                    failOnStatusCode: false,
-                    auth: {
-                        bearer: token,
-                    },
-                }).then((response) => {
-                    expect(response.status).to.equal(204);
-                    expect(response.body).to.equal("");
-                });
-            });
-        });
+        })
     });
 
     it('Não deve ser possível visualizar as informações de uma review feita por um usuário em determinado filme, após a exclusão do usuário', function () {
-
-        cy.login(usuarioCriado).then((login) => {
-            token = login.body.accessToken
-        }).then(function () {
-            cy.promoteAdmin(token).then(function () {
-                cy.createMovie({
-                    title: "Por lugares incríveis",
-                    genre: "Romance/Drama",
-                    description: "Dois adolescentes que estão passando por momentos difíceis criam um forte laço quando embarcam em uma jornada transformadora para visitar as maravilhas do estado de Indiana, nos Estados Unidos.",
-                    durationInMinutes: 108,
-                    releaseYear: 2020
-                }, token).then((movie) => {
+        cy.promoteAdmin(usuarioCriado.accessToken).then(function () {
+            cy.fixture("requests/filmesEvolucaoPerfil.json").then(function (fixture) {
+                cy.createMovie(fixture.porLugaresIncriveis, usuarioCriado.accessToken).then((movie) => {
                     filmeCriado = movie.body
-                }).then(function () {
-                    cy.reviewMovie(filmeCriado.id, 5, "Amei! Adoro filmes de romance dramáticos", token).then(function () {
+                    cy.reviewMovie(filmeCriado.id, 5, "Amei! Adoro filmes de romance dramáticos", usuarioCriado.accessToken).then(function () {
                         cy.request({
                             method: 'DELETE',
-                            url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users/' + usuarioCriado.id,
-                            auth: {
-                                bearer: token,
-                            },
-
+                            url: '/api/users/' + usuarioCriado.id,
+                            auth: { bearer: usuarioCriado.accessToken },
                         }).then(function () {
                             cy.getMovie(filmeCriado.id).then((response) => {
                                 expect(response.status).to.equal(200);
@@ -168,7 +151,7 @@ describe('Exclusão de Usuários', function () {
                         })
                     })
                 });
-            });
+            })
         });
-    }); 
+    });
 });
