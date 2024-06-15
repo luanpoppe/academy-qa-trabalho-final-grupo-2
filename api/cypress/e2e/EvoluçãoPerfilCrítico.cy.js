68bca15dccf8af2689c923b424579e318c3f3bdd
luanpoppe <luanpoppe@gmail.com>
2024-06-15 11:56:48 -0300

    feat: feito revisão e pequenas melhorias nos testes de evolução para perfil crítico na API

--- api/cypress/e2e/EvoluçãoPerfilCrítico.cy.js
+++ api/cypress/e2e/EvoluçãoPerfilCrítico.cy.js
@@ -3,39 +3,41 @@
 
 describe('Evolução de usuário para perfil crítico', function () {
     let usuarioCriado;
-    let token;
+    let adminUser
     let filme;
     let filmeCriado;
 
     beforeEach(function () {
         cy.createUser().then((newUser) => {
             usuarioCriado = newUser;
+            cy.login(usuarioCriado).then(function (resposta) {
+                usuarioCriado.accessToken = resposta.body.accessToken
+            })
         });
     });
 
     before(function () {
-        cy.createUserAndMovie({
-            title: "O Rei Leão",
-            genre: "Animação",
-            description: "As aventuras de um leão jovem de nome Simba, o herdeiro de seu pai, Mufasa.",
-            durationInMinutes: 88,
-            releaseYear: 1994
-        }).then((novoFilme) => {
-            filme = novoFilme.movie.body;
-        });
+        cy.fixture("requests/filmesEvolucaoPerfil.json").then(function (fixture) {
+            cy.createAdminUser().then(function (resposta) {
+                adminUser = resposta
+                cy.createMovie(fixture.reiLeao, adminUser.accessToken).then((novoFilme) => {
+                    filme = novoFilme.body;
+                    cy.createMovie(fixture.reiLeao2, adminUser.accessToken).then((novoFilme) => {
+                        filmeCriado = novoFilme.body;
+                    });
+                })
+            })
+        })
+
     });
 
-    before(function () {
-        cy.createUserAndMovie({
-            title: "O Rei Leão 2: O Reino de Simba",
-            genre: "Animação",
-            description: "Kiara, a filha de Simba e herdeira do reino, desobedece o pai quando ele tenta impedi-la de se meter em confusões.",
-            durationInMinutes: 81,
-            releaseYear: 1998
-        }).then((novoFilme) => {
-            filmeCriado = novoFilme.movie.body;
-        });
-    });
+    after(function () {
+        cy.deleteMovie(filme.id, adminUser.accessToken).then(function () {
+            cy.deleteMovie(filmeCriado.id, adminUser.accessToken).then(function () {
+                cy.deleteUser(adminUser);
+            })
+        })
+    })
 
     afterEach(function () {
         cy.deleteUser(usuarioCriado);
@@ -44,7 +46,7 @@ describe('Evolução de usuário para perfil crítico', function () {
     it('Não deve ser possível evoluir usuário para perfil crítico sem realizar Login', function () {
         cy.request({
             method: 'PATCH',
-            url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users/apply',
+            url: '/api/users/apply',
             failOnStatusCode: false,
         }).then((response) => {
             expect(response.status).to.equal(401);
@@ -56,75 +58,67 @@ describe('Evolução de usuário para perfil crítico', function () {
     });
 
     it('Deve ser possível evoluir usuário Comum para perfil crítico com sucesso', function () {
-        cy.login(usuarioCriado).then((login) => {
-            token = login.body.accessToken
-        }).then(function () {
-            cy.promoteCritic(token).then((response) => {
-                expect(response.status).to.equal(204);
-            })
-        });
+        cy.request({
+            method: 'PATCH',
+            url: '/api/users/apply',
+            auth: {
+                bearer: usuarioCriado.accessToken
+            },
+        }).then((response) => {
+            expect(response.status).to.equal(204);
+        })
     });
 
-
-    it('Deve ser possível evoluir usuário administrador para perfil crítico com sucesso', function () {
-        cy.login(usuarioCriado).then((login) => {
-            token = login.body.accessToken
-        }).then(function () {
-            cy.promoteAdmin(token).then(function () {
-                cy.promoteCritic(token).then((response) => {
-                    expect(response.status).to.equal(204);
-                })
-            });
+    it('Deve ser possível evoluir usuário Administrador para perfil crítico com sucesso', function () {
+        cy.promoteAdmin(usuarioCriado.accessToken).then(function () {
+            cy.request({
+                method: 'PATCH',
+                url: '/api/users/apply',
+                auth: {
+                    bearer: usuarioCriado.accessToken
+                },
+            }).then((response) => {
+                expect(response.status).to.equal(204);
+            })
         })
     });
 
     it('Deve ser possível identificar quando uma review for feita por um usuário Crítico', function () {
-        cy.login(usuarioCriado).then((login) => {
-            token = login.body.accessToken
-        }).then(function () {
-            cy.promoteCritic(token).then(() => {
-                cy.reviewMovie(filme.id, 5, "Melhor filme de animação", token).then(function () {
-                    cy.listReviews(token).then((response) => {
-                        expect(response.status).to.equal(200);
-                        expect(response.body).to.be.an("array");
-                        expect(response.body[0].reviewType).to.equal(1);
-                    })
+        cy.promoteCritic(usuarioCriado.accessToken).then(() => {
+            cy.reviewMovie(filme.id, 5, "Melhor filme de animação", usuarioCriado.accessToken).then(function () {
+                cy.listReviews(usuarioCriado.accessToken).then((response) => {
+                    expect(response.status).to.equal(200);
+                    expect(response.body).to.be.an("array");
+                    expect(response.body[0].reviewType).to.equal(1);
                 })
             })
         });
     });
 
     it('Deve ser possível verificar que as reviews criadas por um usuário crítico impactam nas métricas de avaliação da crítica ', function () {
-        cy.login(usuarioCriado).then((login) => {
-            token = login.body.accessToken
-        }).then(function () {
-            cy.promoteCritic(token).then(function () {
-                cy.reviewMovie(filme.id, 5, "O filme é incrível! Fiquei emocionada", token).then(function () {
-                    cy.getMovie(filme.id).then((response) => {
-                        expect(response.status).to.equal(200);
-                        expect(response.body.id).to.equal(filme.id);
-                        expect(response.body.title).to.equal(filme.title);
-                        expect(response.body.criticScore).to.equal(5);
-                    })
+        cy.promoteCritic(usuarioCriado.accessToken).then(function () {
+            cy.reviewMovie(filme.id, 5, "O filme é incrível! Fiquei emocionada", usuarioCriado.accessToken).then(function () {
+                cy.getMovie(filme.id).then((response) => {
+                    expect(response.status).to.equal(200);
+                    expect(response.body).to.deep.include({
+                        id: filme.id,
+                        title: filme.title,
+                        criticScore: 5,
+                    });
                 })
             })
         });
     });
 
     it('Deve ser possível diferenciar os tipos de reviews feitas por um usuário, tendo ele perfil Comum e depois Crítico', function () {
-
-        cy.login(usuarioCriado).then((login) => {
-            token = login.body.accessToken
-        }).then(function () {
-            cy.reviewMovie(filme.id, 5, "Melhor animação da Disney!", token).then(function () {
-                cy.promoteCritic(token).then(function () {
-                    cy.reviewMovie(filmeCriado.id, 3, "O primeiro filme é muito melhor!", token).then(function () {
-                        cy.listReviews(token).then((response) => {
-                            expect(response.status).to.equal(200);
-                            expect(response.body).to.be.an("array");
-                            expect(response.body[0].reviewType).to.equal(0);
-                            expect(response.body[1].reviewType).to.equal(1);
-                        })
+        cy.reviewMovie(filme.id, 5, "Melhor animação da Disney!", usuarioCriado.accessToken).then(function () {
+            cy.promoteCritic(usuarioCriado.accessToken).then(function () {
+                cy.reviewMovie(filmeCriado.id, 3, "O primeiro filme é muito melhor!", usuarioCriado.accessToken).then(function () {
+                    cy.listReviews(usuarioCriado.accessToken).then((response) => {
+                        expect(response.status).to.equal(200);
+                        expect(response.body).to.be.an("array");
+                        expect(response.body[0].reviewType).to.equal(0);
+                        expect(response.body[1].reviewType).to.equal(1);
                     })
                 })
             });
@@ -132,22 +126,14 @@ describe('Evolução de usuário para perfil crítico', function () {
     });
 
     it('Deve ser possível verificar que as reviews criadas quando um usuário possui perfil comum, não sofrem alteração no seu tipo quando o usuário se torna Crítico', function () {
-
-        cy.login(usuarioCriado).then((login) => {
-            token = login.body.accessToken
-        }).then(function () {
-            cy.reviewMovie(filme.id, 5, "Melhor animação da Disney!", token).then(function () {
-                cy.promoteCritic(token).then(function () {
-                    cy.listReviews(token).then((response) => {
-                        expect(response.status).to.equal(200);
-                        expect(response.body).to.be.an("array");
-                        expect(response.body[0].reviewType).to.equal(0);
-                    })
+        cy.reviewMovie(filme.id, 5, "Melhor animação da Disney!", usuarioCriado.accessToken).then(function () {
+            cy.promoteCritic(usuarioCriado.accessToken).then(function () {
+                cy.listReviews(usuarioCriado.accessToken).then((response) => {
+                    expect(response.status).to.equal(200);
+                    expect(response.body).to.be.an("array");
+                    expect(response.body[0].reviewType).to.equal(0);
                 })
             })
-        });
+        })
     });
 });
-
-
-
